9a710e53ebb330cf1920ac2c163001b1b3b8a915
Kevin Rosen <larrybarnson@gmail.com>
2016-02-15 11:39:29 -0600

    Moved successful canlib.py code into the radar dispatcher

--- src/radar/radar_data_parser.py
+++ src/radar/radar_data_parser.py
@@ -20,6 +20,38 @@ class RadarDataParser(Thread):
         """ Start reading data from the CAN Bus and sending full objects to the dispatcher. """
         # NOTE currently this just generates random numbers and sends them to the
         # dispatcher at random intervals
+	import canlib
+	cl = canlib.canlib()
+	channels = cl.getNumberOfChannels()
+	ch = 0;
+	if ch >= channels:
+	    print("Invalid channel number")
+	    sys.exit()
+	
+	try:
+            ch1 = cl.openChannel(ch, canlib.canOPEN_ACCEPT_VIRTUAL)
+            print("Using channel: %s, EAN: %s" % (ch1.getChannelData_Name(),
+                                              ch1.getChannelData_EAN()))
+
+            ch1.setBusOutputControl(canlib.canDRIVER_NORMAL)
+            ch1.setBusParams(canlib.canBITRATE_500K)
+            ch1.busOn()
+    	except (canlib.canError) as ex:
+            print(ex)
+
+	while True:
+            try:
+            	msgId, msg, dlc, flg, time = ch1.read()
+            	print("%9d  %9d  0x%02x  %d  %s" % (msgId, time, flg, dlc, msg))
+            	for i in range(dlc):
+                    msg[i] = (msg[i]+1) % 256
+                    print(msg, ''.join('{:02x}'.format(x) for x in msg))
+            	ch1.write(msgId, msg, flg)
+            except (canlib.canNoMsg) as ex:
+            	None
+            except (canlib.canError) as ex:
+            	print(ex)
+	'''
         import random
         time.sleep(random.random()*2)
         while True:
@@ -30,3 +62,4 @@ class RadarDataParser(Thread):
 
             self.callback(self, data)
             time.sleep(random.random()*2)
+	'''
