04548b23bc6fb1b87958ab31d65d16ab74d4c9c1
Jon Reynolds <jon.reynolds456@utexas.edu>
2016-02-17 21:25:11 +0000

    converted DSRC message fields to applicable units

--- src/dsrc/dsrc_data_parser.py
+++ src/dsrc/dsrc_data_parser.py
@@ -34,7 +34,7 @@ class DsrcDataParser(Thread):
             root = ElementTree.fromstring(xml)
             message_bytes = root[1].text.split() # Split the bytes on whitespace
 
-            message = {
+            raw_message = {
                 "message_id": int(message_bytes[0], 16), # 1 byte message id
                 "tmp_id": int(''.join(message_bytes[1:5]), 16), # 4 byte tmp id
                 "current_second": int(''.join(message_bytes[5:7]), 16), # 2 byte current seconds
@@ -53,10 +53,46 @@ class DsrcDataParser(Thread):
                 "accel_vert": int(''.join(message_bytes[30:31]), 16), # 1 byte accel vert
                 "accel_yaw": int(''.join(message_bytes[31:33]), 16), # 2 byte accel yaw 
 
-                "breaks": int(''.join(message_bytes[33:35]), 16), # 2 byte break status
+                "brakes": int(''.join(message_bytes[33:35]), 16), # 2 byte brake status
 
                 "vehicle_size": int(''.join(message_bytes[35:38]), 16) # 3 byte vehicle size 
             }
+			
+			message = {
+				"message_id": raw_message["message_id"], # to number 
+				"tmp_id": raw_message["tmp_id"], # unknown
+				"current_second": raw_message["current_second"], # to s
+				
+				"lat": float(raw_message["lat"]) / 1e7, # to minutes.seconds
+				"long": float(raw_message["long"]) / 1e7, # to minutes.seconds
+				"elevation": float(raw_message["elevation"]) / 1e1, # to m
+				"accuracy": raw_message["accuracy"], # unknown 
+				
+				"speed": float(raw_message["speed"]) / 75e3, # to m/s
+				"heading": float(raw_message["heading"]) / 8e1, # unknown
+				"wheel_angle": float(raw_message["wheel_angle"]) * 0.3,
+				
+				"accel_long": float(raw_message["message_id"]) / 14e3, # to m/s^2
+				"accel_lat_set": float(raw_message["accel_lat_set"]) / 3e3, # to m/s^2
+				"accel_vert": float(raw_message["accel_vert"]) / 1e3, # to G's
+				"accel_yaw": float(raw_message["accel_yaw"]) / 175e2, # to degrees/s
+				
+				"brakes": {
+					"aux_brakes": raw_message["brakes"] & 0x03,
+					"brake_boost": (raw_message["brakes"] >> 2) & 0x03,
+					"scs": (raw_message["brakes"] >> 4) & 0x03,
+					"abs": (raw_message["brakes"] >> 6) & 0x03,
+					"traction": (raw_message["brakes"] >> 8) & 0x03,
+					"spare_bit": (raw_message["brakes"] >> 10) & 0x01,
+					"wheel_brakes_unavailable": (raw_message["brakes"] >> 11) & 0x01,
+					"wheel_brakes": (raw_message["brakes"] >> 12) & 0x0F
+				},
+
+				"vehicle_size": {
+					"length": raw_message["vehicle_size"] & 0x3FFF, # to cm
+					"width": (raw_message["vehicle_size"] >> 14) & 0xFF # to cm
+				}
+			}
             
             # If the first line contains Rx then this is a message from a remote DSRC
             # unit, if first_line contains Tx it was a message being sent to a remote DSRC
