4071ed9e1a12f783e317175cd8b3a43fa6778c32
Phillip Lemons <philliplemons512@gmail.com>
2016-02-24 15:18:56 -0600

    Fix script to install opencv

--- videodemo/src/install.sh
+++ videodemo/src/install.sh
@@ -4,60 +4,55 @@
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 _dest="$(readlink -m $_dir/..)"
 
-# Script to install openCV with Python 2.7+
-echo "*** Updating and installing package dependencies ***"
-echo "*** updating your packages! ***"
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install build-essential cmake git pkg-config libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev \
-                libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libatlas-base-dev gfortran
-sudo apt-get -y install python2.7-dev
+echo "*** Installing Dependencies ***"
+sudo apt-get -y update &> /dev/null
+sudo apt-get -y upgrade &> /dev/null
+sudo apt-get -y install build-essential cmake git pkg-config libjpeg8-dev libtiff5-dev libjasper-dev \
+    libpng12-dev libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libatlas-base-dev \
+    gfortran &> /dev/null
+sudo apt-get -y install python2.7-dev &> /dev/null
+
 
 if [ ! $(which pip) ]; then
     echo "*** installing pip ***"
-    wget https://bootstrap.pypa.io/get-pip.py
-    sudo python get-pip.py
+    wget https://bootstrap.pypa.io/get-pip.py &> /dev/null
+    sudo python get-pip.py &> /dev/null
+else
+    echo "pip already installed"
 fi
-
 
 cd $_dest
 if [ ! $(which virtualenv) ]; then
-    echo "*** setting up a virtual environment ***"
-    pip install virtualenv
+    echo "***"
+    pip install virtualenv &> /dev/null
 fi
+echo "*** Done Installing Dependencies ***"
 
+virtualenv cv
+pip install numpy
 
-echo "*** creating an opencv virtual environment ***"
-virtualenv -p /usr/bin/python2.7 cv
-echo "*** installing python NumPy lib ***"
-
-if [ ! -d opencv ]; then
-    echo "*** installing opencv from git ***"
+if [ ! -d $_dest/opencv ]; then
     cd $_dest
     git clone https://github.com/Itseez/opencv.git
     cd $_dest/opencv
     git checkout 3.1.0
-    cd $_dest
 else
     echo "*** opencv already cloned ***"
 fi
 
-if [ ! -d opencv_contrib ]; then
-    echo "*** installing opencv_contrib from git ***"
+if [ ! -d $_dest/opencv_contrib ]; then
     cd $_dest
-    echo $_dest
     git clone https://github.com/Itseez/opencv_contrib.git
     cd $_dest/opencv_contrib
     git checkout 3.1.0
 else
-    echo "*** opencv_contrib already cloned ***"
+    echo "*** opencv_contrib already installed ***"
 fi
 
-
-echo "*** building opencv using cmake ***"
 cd $_dest/opencv
-if [ -d build ]; then
-    rm -rf build/
+if [ -d $_dest/opencv/build ]; then
+    echo "*** Removing old build directory ***"
+    rm -rf $_dest/opencv/build
 fi
 mkdir build
 cd build
@@ -68,26 +63,13 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
     -D OPENCV_EXTRA_MODULES_PATH=$_dest/opencv_contrib/modules \
     -D BUILD_EXAMPLES=ON ..
 
-#cd $_dest/opencv
-#if [ ! -e $_dest/opencv/build ]; then
-#    mkdir $_dest/opencv/build
-#fi
-#cd $_dest/opencv/build
-#cmake -D CMAKE_BUILD_TYPE=RELEASE \
-#	-D CMAKE_INSTALL_PREFIX=/usr/local \
-#	-D INSTALL_C_EXAMPLES=OFF \
-#	-D INSTALL_PYTHON_EXAMPLES=ON \
-#	-D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-#	-D BUILD_EXAMPLES=ON
-
-echo "*** compiling opencv (assumes 4 cores) ***"
 make -j4
 sudo make install
 sudo ldconfig
 
 if [ ! -e $_dest/cv/lib/python2.7/site-packages ]; then
-    echo "*** symlinking opencv to your virtualenv ***"
-    ln -s /usr/local/lib/python2.7/dist-packages/cv2.so $_dest/cv/lib/python2.7/site-packages/
+    echo "*** moving opencv to your virtualenv ***"
+    sudo mv /usr/local/lib/python2.7/site-packages/cv2.so $_dest/cv/lib/python2.7/site-packages/cv2.so
 fi
 
 exit 0
