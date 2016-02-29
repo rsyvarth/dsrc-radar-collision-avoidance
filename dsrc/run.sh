2fa70dfe92843d5a3455867d97e3938330f954d9
Robert Syvarth <robert@duxter.com>
2016-02-29 11:02:53 -0600

    Modify run command to remove messaage limit

--- dsrc/run.sh
+++ dsrc/run.sh
@@ -1,1 +1,1 @@
-sudo D_LEVEL=7 /home/duser/bsm-shell/bsm-shell -c 174 -a 176 -n 174 -x 0x00 -i 2400 --LogUdp --UdpAddr 10.1.1.2 --UdpTxPort 5005
+sudo D_LEVEL=7 /home/duser/bsm-shell/bsm-shell -c 174 -a 176 -n 174 -x 0x00 --LogUdp --UdpAddr 10.1.1.2 --UdpTxPort 5005
