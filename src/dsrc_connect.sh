bf8727ab54713bd6089e900423ee3dbc687b76d1
SunburtReynolds <jon.reynolds456@utexas.edu>
2016-03-25 14:06:27 -0500

    properly killing old bsm processes now

--- src/dsrc_connect.sh
+++ src/dsrc_connect.sh
@@ -86,7 +86,7 @@ echo " - This command polls the CAN bus 10 times/sec for vehicle's speed"
 pass_prompt
 echo "-----------------------------------"
 # Kill running cansend script if one exists
-$sshpass_prefix ssh duser@$ip "sudo pkill cansend"
+$sshpass_prefix ssh duser@$ip "sudo pkill -9 cansend"
 # Start up a new cansend script
 $sshpass_prefix ssh duser@$ip "watch -n0 cansend can0 7DF#02010D0000000000 &"
 echo ""
@@ -97,9 +97,9 @@ echo " - This command starts sending/recieving DSRC messages and logging them to
 pass_prompt
 echo "-----------------------------------"
 # Kill running bsm script if one exists
-$sshpass_prefix ssh duser@$ip "sudo pkill bsm-shell"
+$sshpass_prefix ssh duser@$ip "sudo pkill -9 bsm-shell"
 # Start up a new bsm script
-$sshpass_prefix ssh duser@$ip "sudo D_LEVEL=5 ./bsm-shell -c 174 -a 176 -n 174 -x 0x00 --LogUdp --UdpAddr $self_ip --UdpTxPort 5005 &"
+$sshpass_prefix ssh duser@$ip "sudo D_LEVEL=7 ./bsm-shell -c 174 -a 176 -n 174 -x 0x00 --LogUdp --UdpAddr $self_ip --UdpTxPort 5005"
 echo ""
 
 echo "-----------------------------------"
